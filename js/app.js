f84970312e24029cf36be8eaa0b0c4621502a7e7
Douglas Hernandes <douglashernandes@icloud.com>
2025-06-29 21:42:39 -0300

    criando tabela js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,30 @@
-function buscaApi(){
+async function buscaApi(){
+    const tabela = document.getElementById("tabela-linhas");
+    try{
+        const response = await fetch("https://swapi.info/api/people");
+        const data = await response.json();
+        
+        data.forEach((item) => {
+          const row = tabela.insertRow();
+
+          const name = row.insertCell();
+          name.textContent = item.name;
+
+          const birthYear = row.insertCell();
+          birthYear.textContent = item.birth_year;
+
+          const gender = row.insertCell();
+          gender.textContent = item.gender;
+
+          const hairColor = row.insertCell();
+          hairColor.textContent = item.hair_color;
+
+          const eyeColor = row.insertCell();
+          eyeColor.textContent = item.eye_color;
     
+        });
+    } catch(erro) {
+        console.error("Erro ao retornar dados da API", erro);
+    }
 }
-document.addEventListener("DOMContentLoader", function(){
-    
-})
+document.addEventListener("DOMContentLoader", buscaApi());
